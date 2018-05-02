37aa43cfa75d8e6b3247ff3f0b85e8be96ce1b88
Ray Bell <v-rabell@microsoft.com>
2018-05-02 10:22:33 -0700

    v2

--- Chess/ViewProvider/Program.cs
+++ Chess/ViewProvider/Program.cs
@@ -9,22 +9,19 @@ namespace ViewProvider
 {
     class Program
     {
-s
+
         static void Main(string[] args)
         {
-            //Console.OutputEncoding = System.Text.Encoding.Unicode;
-            //Console.ForegroundColor = ConsoleColor.Black;
-            //MainView.Welcome();
-            //var name = MainView.GetName();
-            //var color = MainView.GetColor();
-            //var curMainView = new MainView(new MainGameApp(new Board(), new TopPlayerSetupProvider(), new HumanPlayer(name, color)));
-            //curMainView.StartDraw();
-            //Console.Read();
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.Black;
+            MainView.Welcome();
+            var name = MainView.GetName();
+            var color = MainView.GetColor();
+            var curMainView = new MainView(new MainGameApp(new Board(), new TopPlayerSetupProvider(), new HumanPlayer(name, color)));
+            curMainView.StartDraw();
+            Console.Read();
 
-            var myTest = new test();
-            var child = new Child();
-           
-           
+
 
 
 
